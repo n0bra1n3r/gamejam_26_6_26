9c490dc79d1e903c086811c3a32ec3c4296d68b0
Jaymari Chua <jace.chua@unsw.edu.au>
2026-06-28 22:06:25 +1000

    Added Door to HighScore scene. Pending fixes.

--- Assets/Scripts/Environment/EnvironmentDoor.cs
+++ Assets/Scripts/Environment/EnvironmentDoor.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using TMPro;
 
-public class NewEmptyCSharpScript
+[RequireComponent(typeof(Collider))]
+public class DoorCollision : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject victoryCanvas;
+    [SerializeField] private TMP_Text congratulationsText;
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private string baseMessage = "Congratulations! Level Cleared!\nCompletion Time: ";
+
+    private float _startTime;
+    private bool _hasTriggered;
+
+    private void Start()
+    {
+        _startTime = Time.time;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_hasTriggered || !collision.gameObject.CompareTag(playerTag)) return;
+
+        _hasTriggered = true;
+
+        float completionTime = Time.time - _startTime;
+
+        congratulationsText.text = $"{baseMessage}{completionTime:F2} seconds";
+        victoryCanvas.SetActive(true);
+    }
 }
